f56e62fc4bacda4888180b8d98279587392f81c3
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-08-02 00:56:17 +0300

    homework#1 done

--- homework#1/index.js
+++ homework#1/index.js
@@ -2,19 +2,47 @@ var list = {
     menu: document.querySelector('.menu'),
     container: document.querySelector('.container'),
     list: document.querySelector('.container__list'),
+    input: document.querySelector('.field__input'),
+    inputValue: '',
+    data: [],
+    dataLength: 0,
 
     setupListener: function() {
         var __this = this;
 
-        this.menu.addEventListener('click', function(event) {
-            event.preventDefault();
+        this.input.addEventListener('keyup', function(event) {
+            var value = event.target.value;
 
-            var target = event.target,
-                action = target.getAttribute('data-action');
+            __this.inputValue = value;
 
-            if (action) {
-                __this[action]();
+            __this.render();
+        });
+    },
+    setupHandleBar: function() {
+        var __this = this;
+
+        Handlebars.registerHelper('sortData', function(data) {
+            var result = '';
+
+            if (__this.inputValue !== '') {
+
+                if (data.toLowerCase().indexOf(__this.inputValue.toLowerCase()) !== -1) {
+
+                    result = new Handlebars.SafeString('<li>' + data + '</li>');
+
+                } else {
+
+                    result = '';
+
+                }
+
+            } else {
+
+                result = new Handlebars.SafeString('<li>' + data + '</li>');
+
             }
+
+            return result;
         });
     },
     load: function() {
@@ -30,7 +58,9 @@ var list = {
                     dataArray.push(data[key].name);
                 }
 
-                __this.render(__this.sort(dataArray));
+                __this.data = __this.sort(dataArray);
+
+                __this.render();
             },
             function() {
                 console.log('Ошибка');
@@ -55,14 +85,17 @@ var list = {
     sort: function(data) {
         return data.sort();
     },
-    render: function(data) {
-        var source = document.getElementById('citiesTemplate').innerHTML,
+    render: function() {
+        var __this = this,
+            source = document.getElementById('citiesTemplate').innerHTML,
             templateFn = Handlebars.compile(source),
-            template = templateFn({list: data});
+            template = templateFn({list: __this.data});
 
         this.container.innerHTML = template;
     },
     init: function() {
+        this.setupHandleBar();
+        this.load();
         this.setupListener();
     }
 };
